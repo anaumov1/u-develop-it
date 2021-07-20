5e548c16aea76fc305315f70e18f3949efc5213b
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-20 11:43:04 -0500

    moved candidate  routes from server to candidates.js

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '1234al',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+
+
+  module.exports = db;
